19243f0942a4835c94636302fe88ff958b57a398
Shubham Jain <100926726+shubhamvkjain@users.noreply.github.com>
2024-09-16 21:59:03 +0530

    login test case completed

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,9 +3,12 @@ const { test, expect } = require('@playwright/test');
 test('register', async ({ page }) => {
 
     await page.goto('https://automationexercise.com/');
+    expect (page)
     await page.locator('[href="/login"]').click();
-    await page.locator('[data-qa="login-email"]').fill('shubhamvkjain@gmail.com');
+    await page.locator('[data-qa="login-email"]').fill('shubhamvkjain1@gmail.com');
     await page.getByPlaceholder('Password').fill('mastershubham');
     await page.locator('[data-qa="login-button"]').click();
+    await page.locator('[href="/delete_account"]').click();
+    expect(page.getByText('Account Deleted!')).toBeVisible();
 
 })
