6a24cb8bde28911e2e982f5cbd65b64c8c1eca8c
Shubham Jain <100926726+shubhamvkjain@users.noreply.github.com>
2024-09-17 20:52:23 +0530

    loginemail change

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,7 +4,7 @@ test('login', async ({ page }) => {
 
     await page.goto('https://automationexercise.com/');
     await page.locator('[href="/login"]').click();
-    await page.locator('[data-qa="login-email"]').fill('shubhamvkjain1@gmail.com');
+    await page.locator('[data-qa="login-email"]').fill('shubhamvkjain53@gmail.com');
     await page.getByPlaceholder('Password').fill('mastershubham');
     await page.locator('[data-qa="login-button"]').click();
     await page.locator('[href="/delete_account"]').click();
